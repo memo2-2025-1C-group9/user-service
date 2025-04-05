7a925d02be98a058b10a60fce69fd5438cd667ba
laufrance <lautaro.fran@gmail.com>
2025-04-05 19:05:20 -0300

    cicd strategy changed

--- app/core/config.py
+++ app/core/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql://user:password@db:5432/student_management"
+    DATABASE_URL: str = "postgresql://user:password@localhost:5432/student_management"
 
 
 settings = Settings()
